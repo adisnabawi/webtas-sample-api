53923900cd38c5638b5e588cd6edf8385cbe9455
Adis <adisazizan@gmail.com>
2023-12-19 18:06:49 +0800

    update

--- app/User.php
+++ app/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function historyLimit()
+    {
+        return $this->hasMany('App\TimeIn')->orderBy('time_in', 'desc')->limit(50);
+    
+    }
 }
