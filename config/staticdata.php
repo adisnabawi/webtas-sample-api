2c2df275488d2321184c77341021b1dc1fb18cc9
Adis <adis.azizan@myeg.com.my>
2024-09-10 17:00:11 +0800

    Type

---
+++ config/staticdata.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'type' => [
+        'staff' => 1,
+        'admin' => 2,
+    ],
+    'history' => [
+        'type' => [
+            'attendance' => 1,
+            'overtime' => 2,
+        ]
+    ]
+
+];
