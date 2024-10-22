f96797c92eb642f7cdec257b44bd81913081cd7b
Adis <adis.azizan@myeg.com.my>
2024-10-22 21:37:49 +0800

    Test

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -10,92 +10,88 @@ class ReportController extends Controller
 {
     public function index(Request $request)
     {
-        $user = $request->user;
-        if ($user->type == config('staticdata.user.type.admin')) {
+        // type(1=hadir,2=takhadir,3=cuti,4=pelulus)
+        $sampleData = [
+            [
+                'name' => 'John Doe',
+                'staff_id' => '12345',
+                'department' => 'IT',
+                'unit' => 'Web Development',
+                'working_hours' => 'WBF',
+                'position' => 'Web Developer',
+                'location_time_in' => '3F Office, Ground Floor',
+                'time_in' => '8:00 AM',
+                'time_out' => '5:00 PM',
+                'late' => '00:00',
+                'time_in_status' => 'On Time',
+                'type' => 1,
+            ],
+            [
+                'name' => 'Jane Doe',
+                'staff_id' => '12346',
+                'department' => 'IT',
+                'unit' => 'Web Development',
+                'working_hours' => 'WBF',
+                'position' => 'Web Developer',
+                'location_time_in' => '3F Office, Ground Floor',
+                'time_in' => '8:00 AM',
+                'time_out' => '5:00 PM',
+                'late' => '00:00',
+                'time_in_status' => 'On Time',
+                'type' => 1,
+            ],
+            [
+                'name' => 'Juan Dela Cruz',
+                'staff_id' => '12347',
+                'department' => 'IT',
+                'unit' => 'Web Development',
+                'working_hours' => 'WBF',
+                'position' => 'Web Developer',
+                'location_time_in' => '',
+                'time_in' => '',
+                'time_out' => '',
+                'late' => '',
+                'time_in_status' => '',
+                'type' => 2,
+            ],
+            [
+                'name' => 'Maria Clara',
+                'staff_id' => '12348',
+                'department' => 'IT',
+                'unit' => 'Web Development',
+                'working_hours' => 'WBF',
+                'position' => 'Web Developer',
+                'location_time_in' => '',
+                'time_in' => '',
+                'time_out' => '',
+                'late' => '',
+                'time_in_status' => '',
+                'type' => 3,
+            ],
+            [
+                'name' => 'Pedro Penduko',
+                'staff_id' => '12349',
+                'department' => 'IT',
+                'unit' => 'Web Development',
+                'working_hours' => 'WBF',
+                'position' => 'Web Developer',
+                'location_time_in' => '3F Office, Ground Floor',
+                'time_in' => '8:00 AM',
+                'time_out' => '5:00 PM',
+                'late' => '00:00',
+                'time_in_status' => 'On Time',
+                'type' => 4,
+            ],
+        ];
 
-            // type(1=hadir,2=takhadir,3=cuti,4=pelulus)
-            $sampleData = [
-                [
-                    'name' => 'John Doe',
-                    'staff_id' => '12345',
-                    'department' => 'IT',
-                    'unit' => 'Web Development',
-                    'working_hours' => 'WBF',
-                    'position' => 'Web Developer',
-                    'location_time_in' => '3F Office, Ground Floor',
-                    'time_in' => '8:00 AM',
-                    'time_out' => '5:00 PM',
-                    'late' => '00:00',
-                    'time_in_status' => 'On Time',
-                    'type' => 1,
-                ],
-                [
-                    'name' => 'Jane Doe',
-                    'staff_id' => '12346',
-                    'department' => 'IT',
-                    'unit' => 'Web Development',
-                    'working_hours' => 'WBF',
-                    'position' => 'Web Developer',
-                    'location_time_in' => '3F Office, Ground Floor',
-                    'time_in' => '8:00 AM',
-                    'time_out' => '5:00 PM',
-                    'late' => '00:00',
-                    'time_in_status' => 'On Time',
-                    'type' => 1,
-                ],
-                [
-                    'name' => 'Juan Dela Cruz',
-                    'staff_id' => '12347',
-                    'department' => 'IT',
-                    'unit' => 'Web Development',
-                    'working_hours' => 'WBF',
-                    'position' => 'Web Developer',
-                    'location_time_in' => '',
-                    'time_in' => '',
-                    'time_out' => '',
-                    'late' => '',
-                    'time_in_status' => '',
-                    'type' => 2,
-                ],
-                [
-                    'name' => 'Maria Clara',
-                    'staff_id' => '12348',
-                    'department' => 'IT',
-                    'unit' => 'Web Development',
-                    'working_hours' => 'WBF',
-                    'position' => 'Web Developer',
-                    'location_time_in' => '',
-                    'time_in' => '',
-                    'time_out' => '',
-                    'late' => '',
-                    'time_in_status' => '',
-                    'type' => 3,
-                ],
-                [
-                    'name' => 'Pedro Penduko',
-                    'staff_id' => '12349',
-                    'department' => 'IT',
-                    'unit' => 'Web Development',
-                    'working_hours' => 'WBF',
-                    'position' => 'Web Developer',
-                    'location_time_in' => '3F Office, Ground Floor',
-                    'time_in' => '8:00 AM',
-                    'time_out' => '5:00 PM',
-                    'late' => '00:00',
-                    'time_in_status' => 'On Time',
-                    'type' => 4,
-                ],
-            ];
-
-            if (!empty($request->type)) {
-                $type = $request->type;
-                $sampleData = array_filter($sampleData, function ($data) use ($type) {
-                    return $data['type'] == $type;
-                });
-            }
-            $sampleData = array_values($sampleData);
-            return response()->json($sampleData, 200);
+        if (!empty($request->type)) {
+            $type = $request->type;
+            $sampleData = array_filter($sampleData, function ($data) use ($type) {
+                return $data['type'] == $type;
+            });
         }
+        $sampleData = array_values($sampleData);
+        return response()->json($sampleData, 200);
     }
 
     public function attendance(Request $request)
